de79e6e0b268d5a67cb9a30c6fee925985f5a5b8
zarar384 <fel98_98@mail.ru>
2024-11-27 22:18:18 +0100

    add Models and json storage/access

---
+++ Data/BlogApiJsonDirectAccess.cs
@@ -0,0 +1,169 @@
+﻿using Data.Models;
+using Data.Models.Interfaces;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Data;
+
+public class BlogApiJsonDirectAccess : IBlogApi
+{
+    private readonly BlogApiJsonDirectAccessSetting _settings;
+    public BlogApiJsonDirectAccess(IOptions<BlogApiJsonDirectAccessSetting> options)
+    {
+        _settings = options.Value;
+
+        ManageDataPaths();
+    }
+
+    private void ManageDataPaths()
+    {
+        CreateDirectoryIfNotExists(_settings.DataPath);
+        CreateDirectoryIfNotExists($@"{_settings.DataPath}\{_settings.BlogPostsFolder}");
+        CreateDirectoryIfNotExists($@"{_settings.DataPath}\{_settings.CategoriesFolder}");
+        CreateDirectoryIfNotExists($@"{_settings.DataPath}\{_settings.TagsFolder}");
+        CreateDirectoryIfNotExists($@"{_settings.DataPath}\{_settings.CommentsFolder}");
+    }
+
+    private async Task<List<T>> LoadAsync<T>(string folder)
+    {
+        var list = new List<T>();
+        foreach(var file in Directory.GetFiles($@"{_settings.DataPath}\{folder}"))
+        {
+            var json = await File.ReadAllTextAsync(file);
+            var item = JsonSerializer.Deserialize<T>(json);
+            if(item is not null)
+            {
+                list.Add(item);
+            }
+        }
+
+        return list;
+    }
+
+    private async Task SaveAsync<T>(string folder, string fileName, T item)
+    {
+        var filePath = $@"{_settings.DataPath}\{folder}\{fileName}.json";
+        await File.WriteAllTextAsync(filePath, JsonSerializer.Serialize<T>(item));
+    }
+
+    private Task DeleteAsync(string folder, string fileName)
+    {
+        var filepath = $@"{_settings.DataPath}\{folder}\{fileName}.json";
+        
+        if (File.Exists(filepath))
+        {
+            File.Delete(filepath);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public async Task<BlogPost?> SaveBlogPostAsync(BlogPost item)
+    {
+        item.Id ??= Guid.NewGuid().ToString();
+        await SaveAsync(_settings.BlogPostsFolder, item.Id, item);
+        return item;
+    }
+    public async Task<Category?> SaveCategoryAsync(Category item)
+    {
+        item.Id ??= Guid.NewGuid().ToString();
+        await SaveAsync(_settings.CategoriesFolder, item.Id, item);
+        return item;
+    }
+    public async Task<Tag?> SaveTagAsync(Tag item)
+    {
+        item.Id ??= Guid.NewGuid().ToString();
+        await SaveAsync(_settings.TagsFolder, item.Id, item);
+        return item;
+    }
+    public async Task<Comment?> SaveCommentAsync(Comment item)
+    {
+        item.Id ??= Guid.NewGuid().ToString();
+        await SaveAsync(_settings.CommentsFolder, item.Id, item);
+        return item;
+    }
+
+    private static void CreateDirectoryIfNotExists(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    public async Task DeleteBlogPostAsync(string id)
+    {
+        await DeleteAsync(_settings.BlogPostsFolder, id);
+
+        var comments = await GetCommentsAsync(id);
+        foreach (var comment in comments)
+        {
+            if (comment.Id != null)
+            {
+                await DeleteAsync(_settings.CommentsFolder, comment.Id);
+            }
+        }
+    }
+
+    public async Task DeleteCategoryAsync(string id)
+    {
+        await DeleteAsync(_settings.CategoriesFolder, id);
+    }
+
+    public async Task DeleteTagAsync(string id)
+    {
+        await DeleteAsync(_settings.TagsFolder, id);
+    }
+
+    public async Task DeleteCommentAsync(string id)
+    {
+        await DeleteAsync(_settings.CommentsFolder, id);
+    }
+
+    public async Task<BlogPost?> GetBlogPostAsync(string id)
+    {
+        var list = await LoadAsync<BlogPost>(_settings.BlogPostsFolder);
+        return list.FirstOrDefault(bp => bp.Id == id);
+    }
+
+    public async Task<int> GetBlogPostCountAsync()
+    {
+        var list = await LoadAsync<BlogPost>(_settings.BlogPostsFolder);
+
+        return list.Count;
+    }
+
+    public async Task<List<BlogPost>> GetBlogPostsAsync(int numberOfPosts, int startIndex)
+    {
+        var list = await LoadAsync<BlogPost>(_settings.BlogPostsFolder);
+        return list.Skip(startIndex).Take(numberOfPosts).ToList();
+    }
+
+    public async Task<List<Category>> GetCategoriesAsync()
+    {
+        return await LoadAsync<Category>(_settings.CategoriesFolder);
+    }
+
+    public async Task<Category?> GetCategoryAsync(string id)
+    {
+        var list = await LoadAsync<Category>(_settings.CategoriesFolder);
+        return list.FirstOrDefault(c => c.Id == id);
+    }
+
+    public async Task<List<Comment>> GetCommentsAsync(string blogPostId)
+    {
+        var list = await LoadAsync<Comment>(_settings.CommentsFolder);
+        return list.Where(t => t.BlogPostId == blogPostId).ToList();
+    }
+
+    public async Task<Tag?> GetTagAsync(string id)
+    {
+        var list = await LoadAsync<Tag>(_settings.TagsFolder);
+        return list.FirstOrDefault(t => t.Id == id);
+    }
+
+    public async Task<List<Tag>> GetTagsAsync()
+    {
+        return await LoadAsync<Tag>(_settings.TagsFolder);
+    }
+}
---
+++ Data/BlogApiJsonDirectAccessSetting.cs
@@ -0,0 +1,10 @@
+﻿namespace Data;
+
+public class BlogApiJsonDirectAccessSetting
+{
+    public string BlogPostsFolder {  get; set; } = string.Empty;
+    public string CategoriesFolder { get; set; } = string.Empty;
+    public string TagsFolder { get; set; } = string.Empty;
+    public string CommentsFolder { get; set; } = string.Empty;
+    public string DataPath { get; set; } = string.Empty;
+}
