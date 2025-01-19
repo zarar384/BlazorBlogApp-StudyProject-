656f04a2e4f831cd1dd59ced901a150ab27ae129
zarar384 <fel98_98@mail.ru>
2025-01-19 21:25:41 +0100

    add Administrator role

--- BlazorWebApp/BlazorWebApp.Client/Program.cs
+++ BlazorWebApp/BlazorWebApp.Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorWebApp.Client;
+using Data.Models.Interfaces;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>(); 
+builder.Services.AddTransient<IBlogApi, BlogApiWebClient>();
 
 builder.Services.AddHttpClient("Api", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
