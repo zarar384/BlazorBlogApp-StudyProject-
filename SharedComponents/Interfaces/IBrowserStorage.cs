7ac971d22da4a49569b7703d4128171751f7124a
zarar384 <fel98_98@mail.ru>
2025-02-03 23:52:02 +0100

    add ShareComponents/IBrowserStorage

---
+++ SharedComponents/Interfaces/IBrowserStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedComponents.Interfaces
+{
+    public interface IBrowserStorage
+    {
+        Task<T?> GetAsnyc<T>(string key);
+        Task SetAsync(string key, object value);
+        Task DeleteAsync(string key);
+    }
+}
